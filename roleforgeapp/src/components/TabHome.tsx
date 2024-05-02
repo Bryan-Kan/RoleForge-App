7794cc1091591dcd8991b5064631a113bc90b328
Bryan-Kan <bryan.lima3@fatec.sp.gov.br>
2024-05-02 16:34:57 -0300

    Drawner

--- roleforgeapp/src/components/TabHome.tsx
+++ roleforgeapp/src/components/TabHome.tsx
@@ -1,48 +1,20 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Home from "../Telas/Home";
 import Mestre from "../Telas/Mestre";
 import Jogador from "../Telas/Jogador";
 import Icoteste from "./Icones";
 
-const Tab = createBottomTabNavigator();
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function TabHome () {
-    return(
-        <Tab.Navigator
-        screenOptions={{
-            headerShown: false,
-            // tabBarActiveBackgroundColor: '#ffff',
-            // tabBarInactiveBackgroundColor: '#121212',
-            tabBarActiveTintColor: '#ffff',
-            tabBarInactiveTintColor: '#ff0000',
-            tabBarStyle:{
-                borderTopWidth: 0,
-                paddingTop: 5,
-                paddingBottom: 5,
-                backgroundColor: '#121212',
-            },
-        }} 
-        initialRouteName='Home'
-        >
-            
-            <Tab.Screen
-            name="Home"
-            component={Home}
-            options={{
-                tabBarLabel: '',
-                tabBarIcon: ({size, color}) => (
-                    <Icoteste/>
-                //     <Entypo name="Home" size={size} color={color}/>
-                )
-            }}
-            />
+const Drawer = createDrawerNavigator();
 
-            <Tab.Screen name="Mestre" component={Mestre}/>
-
-            <Tab.Screen name="Jogador" component={Jogador}/>
-
-        </Tab.Navigator>
-    );
+export default function TabHome() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Mestre" component={Mestre} />
+      <Drawer.Screen name="Jogador" component={Jogador} />
+    </Drawer.Navigator>
+  );
 }
